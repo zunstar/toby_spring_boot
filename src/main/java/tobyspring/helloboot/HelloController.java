8e9c605c8da1edd409b5c1c5cd38928be7880372
zunstar <shimkijun@gmail.com>
2024-10-06 19:51:36 +0900

    의존 오브젝트 추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,8 +1,12 @@
 package tobyspring.helloboot;
 
+import java.util.Objects;
+
 public class HelloController {
 
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
