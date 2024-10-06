f3d0b68b01bef4582d67cb5cf318f8e985098af6
zunstar <shimkijun@gmail.com>
2024-10-06 13:58:20 +0900

    서블릿 컨테이너 띄우기

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,9 +1,14 @@
 package tobyspring.helloboot;
 
 
-public class HellobootApplication {
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+public class HellobootApplication {
     public static void main(String[] args) {
-        System.out.println("Hello Conta!");
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 }
