b3a831832fa7678d89e3ddac1fcc660c421f4de7
zunstar <shimkijun@gmail.com>
2024-10-06 14:15:06 +0900

    서블릿 등록

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,14 +1,30 @@
 package tobyspring.helloboot;
 
 
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+import java.io.IOException;
+
 public class HellobootApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+                    res.setStatus(200);
+                    res.setHeader("Content-Type","text/plain");
+                    res.getWriter().println("Hello Servlet!");
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
     }
 }
