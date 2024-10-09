c77f69f568f06fe751663994943ab0911f4e05c3
zunstar <shimkijun@gmail.com>
2024-10-09 19:24:21 +0900

    자동 구성 애노테이션 적용

--- src/main/java/tobyspring/config/autoconfig/DispatcherServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DispatcherServerConfig.java
@@ -3,8 +3,9 @@ package tobyspring.config.autoconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServerConfig {
 
     @Bean
